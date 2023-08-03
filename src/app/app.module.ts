5dcd88b66b786941849ded4901880f206ebb7826
Pierre Bonny <pierre.bonny@perfect-memory.com>
2023-08-03 18:05:35 +0200

    add sentry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -14,9 +14,29 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserListModule } from './modules/user-list/user-list.module';
 import { HomePageModule } from './modules/home-page/home-page.module';
 import { MediaResearchPageModule } from './modules/media-research-page/media-research-page.module';
-
+import * as Sentry from "@sentry/angular-ivy";
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { Router } from '@angular/router';
 
+Sentry.init({
+  dsn: "https://efbab6022aa575e103a01fcb9754b3c2@o28215.ingest.sentry.io/4505639759052800",
+  integrations: [
+    // Registers and configures the Tracing integration,
+    // which automatically instruments your application to monitor its
+    // performance, including custom Angular routing instrumentation
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.routingInstrumentation,
+    }),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/pierrebonny.github.io\/perfect-project/],
+});
 
 
 @NgModule({
@@ -35,7 +55,18 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     MatTabsModule,
     MediaResearchPageModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: Sentry.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [Sentry.TraceService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
